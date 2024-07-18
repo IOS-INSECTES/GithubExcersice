51e1a1f63081147b75bca9523b287126fa4714cf
OM BHAGWAN <iosproj01@gmail.com>
2024-07-18 22:41:19 +0530

    Initial Commit

---
+++ GithubExcersice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GithubExcersice
+//
+//  Created by OM BHAGWAN on 18/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GithubExcersice/GithubExcersiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubExcersiceApp.swift
+//  GithubExcersice
+//
+//  Created by OM BHAGWAN on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubExcersiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
